14ae5b9b31f1593ea73803a117dbe6fda4e1d8b0
Vivian Phung <dev+github@vivianphung.com>
2023-08-24 19:50:16 -0400

    using ts lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@snowballtools/snowball-ts-framework-template";
+import { exLibFunction } from "@snowballtools/snowball-ts-lib-template";
 
 export default function Home() {
+  useEffect(() => {});
+
   return (
     <>
       <h1>example app</h1>
-      <Button label="Click me!" onClick={() => alert("Button clicked!")} />;
+      <Button
+        label="Click me!"
+        onClick={() => alert(exLibFunction("worked!!!!!!!!"))}
+      />
+      ;
     </>
   );
 }
